beeb8f2469b8c1d2a9938520c9025a1bca272714
LouisBertin <bertin.louis7@gmail.com>
2019-04-01 00:36:15 -0400

    toggle message on button click

--- sandbox/ViewController.swift
+++ sandbox/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cpt: Int = 0
+    @IBOutlet weak var monLabel: UILabel!
+    
+    @IBAction func actionBouton(_ sender: AnyObject) {
+        if (cpt==0) {
+            monLabel.text = "say hello"
+            sender.setTitle("say goodbye", for: .normal)
+            cpt+=1
+        } else {
+            monLabel.text = "say goodbye"
+            sender.setTitle("say hello", for: .normal)
+            cpt-=1
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
